5ffe8f95aa72a789c487715b45ecf17331600ac4
Scott Myers <scott.t.myers@gmail.com>
2022-04-07 06:45:53 -0700

    Added file Metadata

--- src/main/java/com/scottmyers/code/App.java
+++ src/main/java/com/scottmyers/code/App.java
@@ -1,97 +1,116 @@
 package com.scottmyers.code;
-// import java.io.IOException;
+
 import org.apache.commons.cli.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 public class App
 {
-    public static final Option ARG_WATCH = new Option("w", "watch",true, "Set function to Watch Folder with Automation. (Ex: -w /tmp/)");
-    public static final Option ARG_MONITOR = new Option("m", "monitor",true, "Set function to Monitor Folder. (Ex: -w /tmp/)");
+    public static final Option ARG_WATCH = new Option("w", "watch",true, "Set function to Watch Folder with Automation.");
+    public static final Option ARG_MONITOR = new Option("m", "monitor",true, "Set function to Monitor Folder Only. (Ex: -w /tmp/)");
     public static final Option ARG_HELP = new Option("h", "help",false, "Show help for command.");
+    public static final Option ARG_TARGET = new Option("t", "target",true, "Watch & Copy to single target folder.");
+
+    public static FileWriter logFile;
 
     public static Options options = new Options();
 
-    public static void main( String[] args ) throws InterruptedException, ParseException {
+    public static void main( String[] args ) throws IOException, ParseException { // InterruptedException
 
-            ARG_WATCH.setArgs(Option.UNLIMITED_VALUES);
-            options.addOption(ARG_WATCH);
-            options.addOption(ARG_MONITOR);
-            options.addOption(ARG_HELP);
+        initLog();
+        ARG_WATCH.setArgs(Option.UNLIMITED_VALUES);
+        options.addOption(ARG_WATCH);
+        options.addOption(ARG_MONITOR);
+        options.addOption(ARG_HELP);
+        options.addOption(ARG_TARGET);
 
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
 
-            System.out.println("----------------");
+        System.out.println("----------------");
 
-            if (cmd.hasOption("h")) {printHelp(options); System.exit(0);}
-            if ((cmd.hasOption("w")) && (cmd.hasOption("m"))) {
-                System.out.println("Cannot select Watch AND Monitor.");
+        if (cmd.hasOption("h")) {printHelp(options); System.exit(0);}
+
+        if ((cmd.hasOption("w")) && (cmd.hasOption("m"))) {
+            System.out.println("Cannot select Watch AND Monitor.");
+            printHelp(options);
+            System.exit(0);
+        }
+
+        if(cmd.hasOption("w")){
+            String targetPath =(cmd.getOptionValue("t"));
+            if (!Files.exists(Paths.get(targetPath))) {
+                System.out.println("Cannot find Target Folder");
                 printHelp(options);
                 System.exit(0);
             }
 
-            if(cmd.hasOption("w")){
-                String basicList;
-                // StringBuffer strbuffer = new StringBuffer();
-                basicList = (cmd.getOptionValue("w"));
-                String[] argsList = basicList.split(",");
+            String basicList;
+            basicList = (cmd.getOptionValue("w"));
+            String[] argsList = basicList.split(",");
 
-                for (String strBasic : argsList) {
-                    System.out.println("Basic: "+ strBasic);
-
-                    try {
-                        Path path = Paths.get(strBasic.toString());
-                        if (!Files.notExists(path)) {startMe(strBasic);}
-                        else {
-                            System.out.println("!Folder Error:"+ path);
-                            printHelp(options);
-                        }
-                    }
-                    catch (Exception e) {
-                        System.out.println("Folder Error");
-                        // printHelp(options);
+            for (String strBasic : argsList) {
+                // System.out.println("Watching : "+ strBasic);
+                writeLog(strBasic);
+                try {
+                    Path path = Paths.get(strBasic);
+                    if (!Files.notExists(path)) {startMe(strBasic,targetPath);}
+                    else {
+                        System.out.println("!Folder Error:"+ path);
+                        printHelp(options);
                         System.exit(0);
                     }
                 }
+                catch (Exception e) {
+                    System.out.println("Folder Error");
+                    // printHelp(options);
+                    System.exit(0);
+                }
             }
+        }
 
-            if (cmd.hasOption("m")) {
-                String basicList;
-                // StringBuffer strbuffer = new StringBuffer();
-                basicList = (cmd.getOptionValue("m"));
-                String[] argsList = basicList.split(",");
+        if (cmd.hasOption("m")) {
+            String basicList;
+            basicList = (cmd.getOptionValue("m"));
+            String[] argsList = basicList.split(",");
 
-                for (String strBasic : argsList) {
-                    System.out.println("Basic: "+ strBasic);
+            for (String strBasic : argsList) {
+                System.out.println("Basic: "+ strBasic + "--> ");
 
-                    try {
-                        Path path = Paths.get(strBasic.toString());
-                        if (!Files.notExists(path)) {startMe(strBasic);}
-                        else {
-                            System.out.println("!Folder Error:"+ path);
-                            printHelp(options);
-                        }
-                    }
-                    catch (Exception e) {
-                        System.out.println("Folder Error");
-                        // printHelp(options);
+                try {
+                    Path path = Paths.get(strBasic);
+                    if (!Files.notExists(path)) {startMe(strBasic, "");}
+                    else {
+                        System.out.println("!Folder Error: Please check path."+ path);
+                        printHelp(options);
                         System.exit(0);
                     }
                 }
+                catch (Exception e) {
+                    System.out.println("Folder Path Error");
+                    printHelp(options);
+                    System.exit(0);
+                }
             }
-        System.out.println("----------------");
+        }
+        // else {printHelp(options);};
+        // System.out.println("----------------");
+        // logFile.close();
     }
 
 
-    public static void startMe(String path) {
-        FolderThreads folderThread = new FolderThreads(path);
+    public static void startMe(String sourcePath, String targetPath) throws IOException {
+
+        FolderThreads folderThread = new FolderThreads(sourcePath,targetPath);
         folderThread.start();
     }
 
@@ -101,9 +120,28 @@ public class App
         pw.println("\nFolder Monitor v.0.5");
         pw.println();
         formatter.printUsage(pw, 120, "java [-jar] application[.java] [Option] ");
-        formatter.printWrapped(pw, 120, "Example: java App.java -w /tmp/watch-folder ");
+        formatter.printWrapped(pw, 120, "Example: java App.java -w /tmp/watch/1 -t /tmp/output/1");
+        formatter.printWrapped(pw, 120, "Example: java App.java -w /tmp/watch/1, /tmp/watch/2 -t /tmp/output/1");
+        formatter.printWrapped(pw, 120, "Example: java App.java -m /tmp/watch/1");
         pw.println();
         formatter.printOptions(pw, 120, options, 2, 6);
         pw.close();
     }
+
+    public static void initLog() throws IOException {
+        Date d1 = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/YYYY HH:mm a");
+        SimpleDateFormat dfShort = new SimpleDateFormat("HH-mm-a");
+        String formattedDate = df.format(d1);
+        String dfShortFormatted = dfShort.format(d1);
+        String logPathName = "/var/log/WatchService/WatchService-";
+        logFile = new FileWriter(logPathName + dfShortFormatted + ".log",true);
+        logFile.write(formattedDate + " --------------- Application Starting ---------------\n");
+        System.out.println("Log Initiated: " + logPathName + dfShortFormatted + ".log");
+    }
+
+    public static void writeLog(String line) throws IOException {
+        logFile.write(line);
+    }
+
 }
--- src/main/java/com/scottmyers/code/FolderThreads.java
+++ src/main/java/com/scottmyers/code/FolderThreads.java
@@ -1,35 +1,52 @@
 package com.scottmyers.code;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.nio.file.Files.move;
 
 public class FolderThreads extends Thread {
     final private String pathName;
+    public static FileWriter logFile ;
+    public String newPath;
     public static boolean isCtrlCPressed = false;
 
-    public FolderThreads(String pathName){
+    public FolderThreads(String pathName, String newPath) throws IOException {
+
         this.pathName = pathName;
+        this.newPath = newPath;
+        // this.logFile = logFile;
     }
 
     @Override
     public void run() {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line = "---------------------------------\n";
-        System.out.println(line +
-                "Current Thread Name: " + Thread.currentThread().getName() +
-                "\nCurrent Thread ID  : " + Thread.currentThread().getId() +
-                "\nCurrently Watching : " + pathName+
-                "\n");
-
         try {
+            String logPath = "/tmp/";
+            logFile = new FileWriter(logPath + Thread.currentThread().getName()+".log");
+            BufferedWriter logWriter = new BufferedWriter(logFile);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String line = "---------------------------------\n";
+            Date d1 = new Date();
+            SimpleDateFormat df = new SimpleDateFormat("MM/dd/YYYY HH:mm a");
+            String formattedDate = df.format(d1);
+            System.out.println(
+                    // "Date & Time        : "+ formattedDate +
+                    "\nCurrent Thread Name: " + Thread.currentThread().getName() +
+                    // "\nCurrent Thread ID  : " + Thread.currentThread().getId() +
+                    "\nCurrently Watching : " + pathName+
+                    "\n");
+            logWriter.write(formattedDate + "|START|" +"Thread Name:" + Thread.currentThread().getName() +
+                    "|Thread ID:" + Thread.currentThread().getId() +
+                    "|Watching:" + pathName+
+                    "\n");
+            //logWriter.newLine();
             WatchService watchService = null;
             watchService = FileSystems.getDefault().newWatchService();
-
             // Path path = Paths.get(System.getProperty("user.home"));
             Path path = Paths.get(pathName);
-
             path.register(
                     watchService,
                     StandardWatchEventKinds.ENTRY_CREATE,
@@ -37,23 +54,77 @@ public class FolderThreads extends Thread {
                     StandardWatchEventKinds.ENTRY_MODIFY);
 
             WatchKey key;
+
             while ((key = watchService.take()) != null) {
+                Date d2 = new Date();
+                SimpleDateFormat df2 = new SimpleDateFormat("MM/dd/YYYY HH:mm a");
+                String formattedDate2 = df.format(d1);
                 for (WatchEvent<?> event : key.pollEvents()) {
                     System.out.println("-------------------------------------------");
-                    System.out.println("Event kind   : " + event.kind());
-                    System.out.println("Thread Name  : " + Thread.currentThread().getName());
-                    System.out.println("File affected: " + path + "/" + event.context() + "");
-                    System.out.println("-------------------------------------------");
+
+                    if (String.valueOf(event.kind())==("ENTRY_CREATE")) {
+                        String original = (path + "/" + event.context());
+                        String finalDest = (newPath+ "/" + event.context());
+                        File source = new File(original);
+                        File target = new File(finalDest);
+                        Files.copy(source.toPath(), target.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+                        logWriter.write(formattedDate2+"|EVENT|Event kind   : " + event.kind() +
+                                "|Thread Name:" + Thread.currentThread().getName() +
+                                "|File affected:" + path + "/" + event.context() +
+                                "|Copying to:" + finalDest + "\n"
+                                    );
+                        //logWriter.newLine();
+                        System.out.println("Event kind      : " + event.kind());
+                        System.out.println("Thread Name     : " + Thread.currentThread().getName());
+                        System.out.println("Source          : " + path + "/" + event.context() + "");
+                        System.out.println("Target          : " + finalDest);
+                        Path file = Paths.get(target.getAbsolutePath());
+                        BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+
+                        System.out.println("creationTime    : " + attr.creationTime());
+                        System.out.println("lastAccessTime  : " + attr.lastAccessTime());
+                        System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+                        System.out.println("isDirectory     : " + attr.isDirectory());
+                        System.out.println("isOther         : " + attr.isOther());
+                        System.out.println("isRegularFile   : " + attr.isRegularFile());
+                        System.out.println("isSymbolicLink  : " + attr.isSymbolicLink());
+                        System.out.println("Size            : " + attr.size());
+                    }
+
+                    if (String.valueOf(event.kind())==("ENTRY_MODIFY") || (String.valueOf(event.kind())==("ENTRY_DELETE"))) {
+                        logWriter.write(formattedDate2+"|EVENT|Event kind:" + event.kind());
+                        /*+
+                                "|Thread Name:" + Thread.currentThread().getName() +
+                                "|File affected:" + path + "/" + event.context() + "\n");*/
+                       // logWriter.newLine();
+                        }
+
+                    /*System.out.println("Event kind   : " + event.kind() +
+                            "| Thread Name  : " + Thread.currentThread().getName() +
+                            "| File affected: " + path + "/" + event.context() + "\n");
+                    System.out.println("-------------------------------------------");*/
                 }
                 key.reset();
+                logWriter.close();
             }
 
             } catch (IOException e) {
+                System.out.println("IO Exception!");
                 e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            System.exit(0);
             }
-
+            catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
         }
+
+
+
+
+
+
+
+
     }
 
